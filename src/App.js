d77c4f1ebaf4b49b9bde39bb7f264ac15355ca66
ashiktk <ashishgeorgektk575@gmail.com>
2021-06-08 15:14:49 +0530

    set

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Counter from './Counter';
+import {useState} from 'react'
+
+function App() {
+  const [state,setState] = useState(false)
+  return (
+    <div className="App">
+      <h1 onClick={()=>setState(!state)} >Show/Hide</h1>
+      { state && <Counter/> }
+    </div>
+  );
+      }
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+function Header(props) {
+    return(<h1>Hello I am {props.data}</h1>)
+}
+
+export default Header
+
+// import {Component} from 'react'
+
+// class Header extends Component {
+//     render() {
+//         return(<h1>Hello I am {this.props.data}</h1>)
+//     }
+// }
+// export default Header
